909a4c7ed65fe04db09ddfc8cf5c6c7e797c33c4
al3xkras <alexandr.krasovskiy@gmail.com>
2023-03-01 23:16:14 +0100

    index page: display user info

--- src/main/resources/static/js/user/index.js
+++ src/main/resources/static/js/user/index.js
@@ -1,10 +1,6 @@
 
-function getUserInfo() {
+function getUserInfo(xhr_consumer) {
     const username=getCookie(usr_name)
     const token=getCookie(usr_access)
-    sendUserServiceRequest("/user/info?username="+username+"&token="+token,"GET",null,null,xhr=>{
-        console.assert(xhr instanceof XMLHttpRequest)
-        //xhr=new XMLHttpRequest(xhr)
-        console.log(xhr.responseText)
-    })
+    sendUserServiceRequest("/user/info?username="+username+"&token="+token,"GET",null,null,xhr_consumer)
 }
