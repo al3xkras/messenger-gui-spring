e468648e5656dd2508acf8689a124254e24966a3
al3xkras <alexandr.krasovskiy@gmail.com>
2023-03-02 14:16:30 +0100

    index.html: implement chat list; UserController.java: add endpoint, (GET /chat) - get chat info

--- src/main/java/com/al3xkras/messenger/user_service/controller/UserController.java
+++ src/main/java/com/al3xkras/messenger/user_service/controller/UserController.java
@@ -90,5 +90,35 @@ public class UserController {
         return response;
     }
 
+    @GetMapping("/chat")
+    @ResponseBody
+    public Object getChatInfo(@RequestParam(value = "chat-id", required = false) Long chatId,
+                              @RequestParam(value = "chat-name",required = false) String chatName,
+                              @RequestParam(value = "token") String token) throws URISyntaxException {
+
+        String uri = MessengerUtils.Property.CHAT_SERVICE_URI.value();
+        log.info("token: "+token);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", MediaType.APPLICATION_JSON.toString());
+        headers.add("Accept", MediaType.APPLICATION_JSON.toString());
+        headers.add("Authorization", token);
+
+        URIBuilder uriBuilder = new URIBuilder(uri+"/chat");
+        if (chatId!=null)
+            uriBuilder.addParameter("chat-id",chatId.toString());
+        if (chatName!=null)
+            uriBuilder.addParameter("chat-name",chatName);
+
+        RequestEntity<PageRequestDto> request = new RequestEntity<>(
+                headers,
+                HttpMethod.GET,
+                uriBuilder.build()
+        );
+        Object response = restTemplate.exchange(request, Object.class);
+        log.info("response: "+response);
+        return response;
+
+    }
 
 }
--- src/main/resources/static/js/cookies.js
+++ src/main/resources/static/js/cookies.js
@@ -29,3 +29,75 @@ function getCookie(name) {
 function eraseCookie(name) {
     document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
+
+function initUserServiceTokens(username,password){
+    const login_form = new FormData();
+    login_form.append( "username",username)
+    login_form.append("password",password)
+    const xhr = new XMLHttpRequest()
+    xhr.overrideMimeType("application/json")
+    xhr.open('POST', '/login', true);
+    xhr.onload=()=>{
+        if (xhr.status===200){
+            const response = JSON.parse(xhr.responseText)
+            console.log(JSON.stringify(response))
+            const access=response['access-token']
+            const refresh=response['refresh-token']
+
+            if (access){
+                setCookie(usr_name,username,7)
+                setCookie(usr_access,"Bearer "+access,7)
+                document.location="/user/index"
+            } else {
+                return
+            }
+            if (refresh){
+                //todo implement
+            }
+        } else {
+            alert("failed to log in")
+        }
+    }
+    xhr.send(login_form)
+}
+
+function initChatServiceTokens(chat_name,hide_alert){
+    const user_token = getCookie(usr_access)
+    const login_form = new FormData();
+    login_form.append( "user-access-token",user_token)
+    if (chat_name)
+        login_form.append("chat-name",chat_name)
+    let cookie_name=nochat_access
+    if (chat_name)
+        cookie_name=chat_access
+    const xhr = new XMLHttpRequest()
+    xhr.overrideMimeType("application/json")
+    xhr.open('POST', '/chat/auth', true);
+    xhr.onload=()=>{
+        if (xhr.status===200){
+            const response = JSON.parse(xhr.responseText)
+            console.log(JSON.stringify(response))
+            const access=response['access-token']
+            const refresh=response['refresh-token']
+
+            if (access){
+                setCookie(cookie_name,"Bearer "+access,7)
+                if (!hide_alert) {
+                    alert("auth successful")
+                    document.location="/user/index"
+                }
+                console.log("auth successful")
+            } else {
+                alert("access token not received")
+                return
+            }
+            if (refresh){
+                //todo implement
+            }
+        } else {
+            if (!hide_alert) alert("failed to authenticate")
+            console.log("auth failed")
+        }
+    }
+    xhr.send(login_form)
+}
